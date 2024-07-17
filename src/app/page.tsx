ee2a4d627669e18520415b8319123f9bcd2f0320
AbhimanAv01 <abhimanav06@gmail.com>
2024-07-17 16:26:47 +0530

    first

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import Navbar from "./components/header/navbar";
 import Footer from "./components/footer/Footer";
 import AboutSection from "./components/about/About";
 import Topabout from "./components/topabout/Topabout"
-import Experience from "./components/experiences/Experiences";
 import Education from "./components/Education/Education";
 import  Projects  from "./components/projects/Projetcs";
 import Contactus from "./components/contactus/contactus";
